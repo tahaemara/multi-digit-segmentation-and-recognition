bc4143dcec6bf6d43f673c59fa37872a56f86034
Taha Emara <tahaemara.eng@gmail.com>
2018-02-14 12:21:08 +0200

    add main class

---
+++ src/main/java/com/emaraic/digitrecognition/Application.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.emaraic.digitrecognition;
+
+/**
+ *
+ * @author Taha Emara
+ * Website: http://www.emaraic.com
+ * Email  : taha@emaraic.com
+ * Created on: Feb 14, 2018 
+ */
+public class Application {
+
+}
