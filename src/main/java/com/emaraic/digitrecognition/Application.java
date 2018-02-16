7bc53b458da046732ba9194cd093343a1966f8d7
Taha Emara <tahaemara.eng@gmail.com>
2018-02-16 15:54:55 +0200

    correct path to sample and rename images

--- src/main/java/com/emaraic/digitrecognition/Application.java
+++ src/main/java/com/emaraic/digitrecognition/Application.java
@@ -63,7 +63,7 @@ import org.nd4j.linalg.factory.Nd4j;
 public class Application {
 
     private static MultiLayerNetwork restored;
-    private final static String IMAGEPATH = "samples/rec.jpg";
+    private final static String IMAGEPATH = "samples/sample1.jpg";
     private final static String[] DIGITS = {"0", "1", "2", "3", "4", "5", "6", "7", "8", "9"};
 
     public Application() {
